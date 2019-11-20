3d163bf479acb93045772525ba2d4b6cccb54d1f
graniter <guguste362@gmail.com>
2019-11-20 23:54:56 +0100

    Added an asteroid following the physics of gravity

---
+++ Assets/Scripts/AsteroidManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AsteroidManager : MonoBehaviour
+{
+
+    public Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.AddForce(-transform.position.normalized / transform.position.sqrMagnitude);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        Destroy(gameObject);
+    }
+}
