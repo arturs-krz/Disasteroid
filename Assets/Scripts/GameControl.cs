553d83342154c5bac3b1436c8f61b8bcf05a7713
Nahida Islam <nahida@kth.se>
2019-12-07 13:15:56 +0100

    Revert "Merge branch 'master' into buying-mechanism"
    This reverts commit 2891b409e123514ec9997458ec809c6c01cddc96, reversing
    changes made to 38539ad8975bd028eedb3bfad8579470a5df02db.

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
@@ -9,19 +9,16 @@ public class GameControl : MonoBehaviour
     public Slider PD;
     public Slider CO2;
     int moneyValue;
-    double bombPrice = 10e5;
+    double bombPrice = 10e7;
     public Text userDisplay;
     public Text moneyDisplay;
     public GameObject gameover;
 
     //color change for sliders
     public int MaxVal = 100;
+    public Image Fill;  // assign in the editor the "Fill"
     public Color MaxColor;
     public Color MinColor;
-
-
-    private Image populationSliderFill;
-    private Image CO2SliderFill;
 
     // Start is called before the first frame update
     void Start()
@@ -34,23 +31,23 @@ public class GameControl : MonoBehaviour
 
         //userDisplay.text = PlayerPrefs.GetString("Username").ToString() + " is playing!";
         //userDisplay.text = PlayerPrefs.GetString("Username").ToString();
-  
+
         moneyValue = (int)(PD.value * 10e8);
-        moneyDisplay.text = "$ " + moneyValue.ToString("n0");
+        moneyDisplay.text = moneyValue.ToString();
         PD.minValue = 0f;
         PD.maxValue = MaxVal;
         CO2.minValue = 0f;
         CO2.maxValue = MaxVal;
-        CO2.value = CO2.maxValue/2;
-        PD.value = PD.maxValue/2;
+        CO2.value = PD.minValue;
+        PD.value = PD.minValue;
     }
 
     public void BombAttack()
     {
-        PD.value -= PD.maxValue / 20;
-        CO2.value += MaxVal / 10;
-        moneyValue = (int)(moneyValue + PD.value * 10e6  - bombPrice);
-        moneyDisplay.text = "$ " + moneyValue.ToString("n0");
+        PD.value += PD.maxValue / 10;
+        CO2.value += 100 / 5;
+        moneyValue = (int)(PD.value * 10e8 - bombPrice);
+        moneyDisplay.text = moneyValue.ToString();
     }
 
     public void SatelitteScan()
@@ -77,11 +74,11 @@ public class GameControl : MonoBehaviour
         Color colorPD = Color.Lerp(MinColor, MaxColor, (float)PD.value / MaxVal);
         PD.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = colorPD;
 
-        CO2.value -= Time.deltaTime * (PD.maxValue - PD.value) * 0.001f;
+        CO2.value += Time.deltaTime * (PD.maxValue - PD.value) * 0.001f;
         Color colorCO2 = Color.Lerp(MinColor, MaxColor, (float)CO2.value / MaxVal);
         CO2.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = colorCO2;
 
-        moneyDisplay.text = "$ " + moneyValue.ToString("n0");
+        moneyDisplay.text = moneyValue.ToString();
         // if (PD.value >= PD.maxValue || WL.value >= WL.maxValue || CO2.value >= CO2.maxValue)
         // {
         //     GameOver();
